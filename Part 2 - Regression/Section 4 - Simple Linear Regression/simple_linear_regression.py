7c24045db7659219c4faac8a2d38e6c6f0e093c8
adenosinew <sage.woo@gmail.com>
2017-12-30 14:26:24 -0500

    did simple linear regression part

--- Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
+dataset = pd.read_csv('/home/kexing/Code_practice/machine_learning_az/Part 2 - Regression/Section 4 - Simple Linear Regression/Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
